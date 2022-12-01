e61c2ff3e2bb68c08ec232f84368ce63b0baaf56
Luis Hernández Gómez <ing.luishg76@gmail.com>
2022-12-01 00:50:04 -0500

    salva(01-12-022)X

--- drag_and_drop/example_drag_and_drop_colors.py
+++ drag_and_drop/example_drag_and_drop_colors.py
@@ -2,7 +2,70 @@ import flet as ft
 from flet import *
 
 def main(page:Page):
-    page.title="Drag and drop colors example"
+    page.title="Drag and drop colors example"   
 
+    def drag_will_accept(e):
+        e.control.content.border=border.all(2,colors.RED
+        if e.data=="true" else colors.BLACK45)
+        e.control.update()
+
+    def drag_accept(e):
+        source=page.get_control(e.src_id)
+        e.control.content.bgcolor=source.content.bgcolor
+        e.control.content.border=border.all(1,colors.BLACK45)
+        e.control.update()
+
+    def drag_leave(e):
+        e.control.content.border=border.all(1,colors.BLACK45)
+        e.control.update()
+
+    def container_color(bgcolor:colors,bcolor=colors.BLACK45):
+        return Container(
+                            bgcolor=bgcolor,
+                            border=border.all(1,bcolor),
+                            width=50,
+                            height=50,
+                            border_radius=border_radius.all(5)
+                        )
+    def container_feedback_color(bgcolor:colors,bcolor=colors.BLACK45):
+        return Container(
+                            bgcolor=bgcolor,
+                            border=border.all(1,bcolor),
+                            width=20,
+                            height=20,
+                            border_radius=border_radius.all(3)
+                        )
+    page.add(Row(
+         [Column(
+            [
+                Draggable(
+                    group="colors",
+                    content=container_color(colors.BLUE),
+                    content_feedback=container_feedback_color(colors.BLUE)
+                    ),
+                Draggable(
+                    group="colors",
+                    content=container_color(colors.YELLOW),
+                    content_feedback=container_feedback_color(colors.YELLOW)
+                    ),
+                Draggable(
+                    group="colors",
+                    content=container_color(colors.GREEN),
+                    content_feedback=container_feedback_color(colors.GREEN)
+                    )
+            ]
+         ),
+         Column([Container(width=100)]),
+         Column([
+                  DragTarget(
+                    group="colors",
+                    content=container_color(colors.WHITE10),
+                    on_will_accept=drag_will_accept,
+                    on_leave=drag_leave,
+                    on_accept=drag_accept
+                    )
+                ]
+         )]
+    ))
     
-    #def on_accept(e):
+app(target=main)  
--- drag_and_drop/example_drag_and_drop_number.py
+++ drag_and_drop/example_drag_and_drop_number.py
@@ -5,37 +5,46 @@ def main(page:Page):
     page.title="Drag and Drop example"
 
     def on_accept(e):
-        source=page.get_control(e.src_id)
-        source.content.content.value='0'
-        source.content.bgcolor=colors.PINK_200
-        e.control.content.content.value='1'
-        e.control.content.bgcolor=colors.CYAN_200
-        page.update()
+       source=page.get_control(e.src_id)
+       source.content.content.value='0'
+       source.content.bgcolor=colors.PINK_200
+       e.control.content.content.value='1'
+       e.control.content.bgcolor=colors.CYAN_200
+       page.update()
 
-    page.add(Row([Draggable(
-                    group="number",
-                    content=Container(
-                        width=50,
-                        height=50,
-                        bgcolor=colors.CYAN_200,
-                        border_radius=border_radius.all(5),
-                        content=Text("1", size=20),
-                        alignment=alignment.center,
-                    )
+    page.add(
+        Row([
+             Draggable(  
+                group="number",
+                content=Container(
+                    width=50,
+                    height=50,
+                    bgcolor=colors.CYAN_200,
+                    border_radius=border_radius.all(5),
+                    content=Text("1", size=20),
+                    alignment=alignment.center
+                ),      
+                content_when_dragging=Container(
+                                        width=50,
+                                        height=50,
+                                        bgcolor=colors.BLUE_GREY_200,
+                                        border_radius=border_radius.all(5)
+                ),
+                content_feedback=Text("1",size=20)                     
                 ),
                 Container(width=100),
                 DragTarget(
                     group="number",
                     content=Container(
-                        width=50,
-                        height=50,
-                        bgcolor=colors.PINK_200,
-                        border_radius=border_radius.all(5),
-                        content=Text("0", size=20),
-                        alignment=alignment.center,
-                    ),
-                    on_accept=on_accept
-                )])
-    )  
-
+                            width=50,
+                            height=50,
+                            bgcolor=colors.PINK_200,
+                            border_radius=border_radius.all(5),
+                            content=Text("0", size=20),
+                            alignment=alignment.center,
+                        ),
+                on_accept=on_accept
+                )            
+            ])
+        )
 app(target=main)
